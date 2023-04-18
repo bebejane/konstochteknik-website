fb500f5a92ba75f930262ceeebf1f981e07ca74d
Bebe Jane <bjornthief@gmail.com>
2023-04-18 08:51:45 +0200

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,14 @@ import { AllProjectsDocument } from '$graphql';
 import { BYPASS_TOKEN } from '$env/static/private';
 import client from '../client'
 
+export const config = {
+  isr: {
+    expiration: 30,
+    bypassToken: BYPASS_TOKEN,
+  }
+};
+
+
 export const load = (async ({ params }) => {
   const data = await client.request(AllProjectsDocument)
   return {
