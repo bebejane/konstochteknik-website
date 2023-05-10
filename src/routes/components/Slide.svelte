532239492b0c54d3eb1ad8db76fd3404aa189726
konst-teknik <information-konst-teknik.se>
2023-05-10 14:29:12 +0200

    Fix

--- src/routes/components/Slide.svelte
+++ src/routes/components/Slide.svelte
@@ -26,18 +26,23 @@
 			left: 50%;
 			bottom: 0;
 			transform: translateX(-50%);
-			margin-bottom: var(--other-margin);
-			padding: 0.9rem;
-			padding-bottom: 0.5rem;
+			margin-bottom: calc(1.5 * var(--other-margin));
+			padding-left: 1rem;
+			padding-right: 1rem;
+			padding-top: 0.8rem;
+			padding-bottom: 0rem;
 			line-height: 1;
 			border-radius: 3rem;
 			color: var(--black);
 			z-index: 10;
-			width: 100%;
 			text-align: center;
+			@include mq($from: tablet) {
+				white-space: nowrap;
+			}
 			@include mq($until: tablet) {
 				line-height: calc(0.9 * var(--line-height));
 				border-radius: 1rem;
+				width: 100%;
 			}
 
 			&.fill {
