c9b690d7999cba9698fd23c219794d1a0e0bbf22
Bebe Jane <bjornthief@gmail.com>
2025-06-28 09:36:27 +0200

    fix2

--- app/components/slides/VideoSlide.tsx
+++ app/components/slides/VideoSlide.tsx
@@ -1,42 +1,38 @@
-'use client'
+'use client';
 
-import { useEffect, useRef } from "react";
-import s from "./VideoSlide.module.scss";
+import { useEffect, useRef } from 'react';
+import s from './VideoSlide.module.scss';
 import parseStyles from 'css-to-style';
 
 type Props = {
-  active: boolean
-  data: VideoSlideRecord
-}
+	active: boolean;
+	data: VideoSlideRecord;
+};
 export default function VideoSlide({ data: { backgroundImage, video, poster, id, css }, active }: Props) {
+	const ref = useRef<HTMLVideoElement>(null);
 
-  const ref = useRef<HTMLVideoElement>(null)
+	useEffect(() => {
+		if (active) {
+			ref.current.currentTime = 0;
+			ref.current.play();
+		} else ref.current.pause();
+	}, [active]);
 
-  useEffect(() => {
-
-    if (active) {
-      ref.current.currentTime = 0;
-      ref.current.play();
-    } else
-      ref.current.pause()
-
-  }, [active])
-
-  return (
-    <div
-      className={s.videoslide}
-      style={{
-        backgroundImage: backgroundImage ? `background-image:url(${backgroundImage?.url}?w=2000)` : '',
-        ...parseStyles(css ?? '')
-      }}
-    >
-      <div className={s.monitor}>
-        <img src="/images/monitor.png" alt="monitor" />
-        <video ref={ref} poster={poster?.url} muted playsInline>
-          <source src={video.video.mp4Url} type="video/mp4" />
-          <track kind="captions" />
-        </video>
-      </div>
-    </div>
-  )
+	return (
+		<div
+			className={s.videoslide}
+			style={{
+				backgroundImage: backgroundImage ? `url(${backgroundImage?.url}?w=2000)` : '',
+				...parseStyles(css ?? ''),
+			}}
+		>
+			<div className={s.monitor}>
+				<img src='/images/monitor.png' alt='monitor' />
+				<video ref={ref} poster={poster?.url} muted playsInline>
+					<source src={video.video.mp4Url} type='video/mp4' />
+					<track kind='captions' />
+				</video>
+			</div>
+		</div>
+	);
 }
