baef0ee29232a7ea008da24d9330fc0982fd99de
Bebe Jane <bjornthief@gmail.com>
2025-10-10 18:34:02 +0200

    jakul

--- components/Thumbnails.tsx
+++ components/Thumbnails.tsx
@@ -23,19 +23,23 @@ const octave = 5;
 export default function Thumbnails({ allProjects }: Props) {
 	const swiperRef = useRef<Swiper | null>(null);
 	const toneRef = useRef<Tone.DuoSynth | null>(null);
+	const masterRef = useRef<Tone.Channel | null>(null);
+
 	const [hover, setHover] = useState<string | null>(null);
 	const [category, index, setIndex] = useStore(useShallow((s) => [s.category, s.index, s.setIndex]));
 	const projects = allProjects.filter(({ category: cat }) => !category || cat === category);
 
 	function initTone() {
 		if (toneRef.current) return;
-		const feedbackDelay = new Tone.FeedbackDelay('2n', 0.9).toDestination();
-		const phaser = new Tone.Phaser(2, 1, 200).toDestination();
-		const volume = new Tone.Gain(-2, 'gain').toDestination();
+		if (toneRef.current) toneRef.current.dispose();
 
-		feedbackDelay.wet.value = 0.8;
+		const feedbackDelay = new Tone.FeedbackDelay('2n', 0.9);
+		const phaser = new Tone.Phaser(2, 1, 200);
+
+		feedbackDelay.wet.value = 0.1;
 		phaser.wet.value = 0.3;
 
+		masterRef.current = new Tone.Channel(-10, 0).toDestination();
 		toneRef.current = new Tone.DuoSynth({
 			volume: 0.1,
 			harmonicity: 0.5,
@@ -44,23 +48,19 @@ export default function Thumbnails({ allProjects }: Props) {
 			context: {
 				latencyHint: 'interactive',
 			},
-		})
-			.chain(feedbackDelay, phaser, volume)
-			.toDestination();
+		}).chain(feedbackDelay, phaser, masterRef.current);
 	}
 
 	function playNote() {
 		const piano = notes.map((n, i) => `${n}${octave}`).concat(notes.map((n, i) => `${n}${octave + 1}`));
 		const idx = Math.floor(projects.findIndex((p) => p.id === hover) % piano.length);
-
 		toneRef.current?.triggerAttackRelease(piano[idx], 3, Tone.now() + 0.05, 0.005);
 		console.log('play note', piano[idx]);
 	}
 
-	useEffect(() => {
-		//if (hover === null) return;
-		//toneRef.current?.triggerAttackRelease('C5', 1, Tone.now(), 1);
-	}, [hover]);
+	function playShortNote() {
+		toneRef.current?.triggerAttackRelease('C5', 0.05, Tone.now(), 0.005);
+	}
 
 	return (
 		<>
@@ -97,7 +97,10 @@ export default function Thumbnails({ allProjects }: Props) {
 							setIndex(idx);
 							setHover(null);
 						}}
-						onMouseEnter={() => setHover(p.id)}
+						onMouseEnter={() => {
+							playShortNote();
+							setHover(p.id);
+						}}
 						onMouseLeave={() => setHover(null)}
 					>
 						<img src={`${p.thumbnail.url}?w=400`} alt={p.title} className={s.thumbnail} />
