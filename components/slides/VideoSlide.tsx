8daed3e10900fd6172768391fcab7b485191b055
Bebe Jane <bjornthief@gmail.com>
2026-04-14 10:21:25 +0200

    ixar

--- components/slides/VideoSlide.tsx
+++ components/slides/VideoSlide.tsx
@@ -1,28 +1,40 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { CSSProperties, useEffect, useRef, useState } from 'react';
 import s from './VideoSlide.module.scss';
 import parseStyles from 'css-to-style';
 
 type Props = {
 	active: boolean;
 	data: VideoSlideRecord;
+	onLoad(): void;
 };
 
-export default function VideoSlide({ data: { backgroundImage, video, poster, css } }: Props) {
+export default function VideoSlide({
+	data: { backgroundImage, video, poster, css },
+	onLoad,
+}: Props) {
 	const ref = useRef<HTMLVideoElement>(null);
+	const [loaded, setLoaded] = useState(false);
 
 	return (
 		<div
 			className={s.videoslide}
 			style={{
 				backgroundImage: backgroundImage ? `url(${backgroundImage?.url}?w=2000)` : '',
-				...parseStyles(css ?? ''),
+				...(parseStyles(css ?? '') as CSSProperties),
 			}}
 		>
 			<div className={s.monitor}>
 				<img src='/images/monitor.png' alt='monitor' />
-				<video ref={ref} poster={poster?.url} muted={true} autoPlay={true} playsInline>
+				<video
+					ref={ref}
+					poster={poster?.url}
+					muted={true}
+					autoPlay={true}
+					playsInline
+					onCanPlay={() => onLoad()}
+				>
 					<source src={video.video.mp4Url} type='video/mp4' />
 					<track kind='captions' />
 				</video>
