eecab002106c1fd298cd40a41eecf134f1079adf
konst-teknik <information@konst-teknik.se>
2026-04-15 08:50:32 +0200

    Fix

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -31,11 +31,12 @@ export default function Intro({ intro }: Props) {
 	if (!show) return null;
 	return (
 		<div className={s.intro} onClick={() => setShow(false)}>
-			<h1>
+			<h1 className="big">
 				<span className={s.title}>{loader.title}</span>
-				<span className={s.and}>&</span>
+				<span className={s.and}><i>&</i></span>
 				<span className={s.subtitle}>{loader.subtitle}</span>
 			</h1>
+			<h2><i>Loading...</i></h2>
 		</div>
 	);
 }
