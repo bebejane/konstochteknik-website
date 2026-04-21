f33aa60270059b44c12d1d53df6a451202ed7e8a
Bebe Jane <bjornthief@gmail.com>
2026-04-21 14:32:06 +0200

    loading

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -1,34 +1,33 @@
-import { apiQuery } from 'next-dato-utils/api';
-import { AllProjectsDocument, ProjectDocument } from '@/graphql';
-import { notFound } from 'next/navigation';
-import Slide from '@/components/slides';
-import { DraftMode } from 'next-dato-utils/components';
+import { apiQuery } from "next-dato-utils/api";
+import { AllProjectsDocument, ProjectDocument } from "@/graphql";
+import { notFound } from "next/navigation";
+import Slide from "@/components/slides";
+import { DraftMode } from "next-dato-utils/components";
 
 export type ProjectPageProps = {
-	params: Promise<{ project: string }>;
+  params: Promise<{ project: string }>;
 };
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
-	const { project: slug }: { project: string } = await params;
+  const { project: slug }: { project: string } = await params;
 
-	const { project, draftUrl } = await apiQuery(ProjectDocument, {
-		variables: { slug },
-	});
+  const { project, draftUrl } = await apiQuery(ProjectDocument, {
+    variables: { slug },
+  });
 
-	if (!project) return notFound();
+  if (!project) return notFound();
 
-	return (
-		<>
-			<Slide project={project} single={true} />
-			<DraftMode url={draftUrl} path={`/projects/${slug}`} />
-		</>
-	);
+  return (
+    <>
+      <Slide project={project} single={true} />
+      <DraftMode url={draftUrl} path={`/projects/${slug}`} />
+    </>
+  );
 }
 
 export async function generateStaticParams() {
-	const { allProjects } = await apiQuery(AllProjectsDocument, {
-		all: true,
-		tags: ['project'],
-	});
-	return allProjects.map(({ slug: project }) => ({ project }));
+  const { allProjects } = await apiQuery(AllProjectsDocument, {
+    all: true,
+  });
+  return allProjects.map(({ slug: project }) => ({ project }));
 }
